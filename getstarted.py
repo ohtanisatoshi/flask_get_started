b2f10b76d9eaf0e2e330df738ea0603048271008
satoshi.otani.fc <satoshi.otani.fc@hitachi-solutions.com>
2017-01-12 21:07:31 +0900

    update

--- getstarted.py
+++ getstarted.py
@@ -12,11 +12,18 @@ model = gensim.models.doc2vec.Doc2Vec.load(model_file)
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'GET':
-        return render_template('index.html')
+        table_data = []
+        return render_template('index.html', table_data=table_data)
     else:
         search_text = request.form['search-text'].encode('utf-8')
-        parsed_text = mt.parse(search_text).decode('utf-8')
-        return render_template('index.html', parsed_text=parsed_text)
+        test_doc = gensim.utils.simple_preprocess(mt.parse(search_text))
+        inferred_vector = model.infer_vector(test_doc)
+        sims = model.docvecs.most_similar([inferred_vector], topn=5)
+        table_data = []
+        for j, t in enumerate(sims):
+            table_data.append([t[0], "{:2f}".format(t[1])])
+
+        return render_template('index.html', table_data=table_data)
 
 if __name__ == '__main__':
     app.run()
